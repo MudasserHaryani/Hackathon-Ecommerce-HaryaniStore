d49cf763d5f414108a281fbc0eaf3a2e02fcaa7b
Mudasser Haryani <160148229+MudasserHaryani@users.noreply.github.com>
2025-02-08 00:25:32 +0500

    Add files via upload

---
+++ app/(store)/page.tsx
@@ -0,0 +1,24 @@
+import GoodFridayBanner from "@/components/GoodFridayBanner";
+import ProductsView from "@/components/ProductsView";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
+
+export const dynamic = "force-static";
+export const revalidate = 60; // revalidate at most every 60 seconds 
+export default async function Home() {
+  const products = await getAllProducts();
+const categories = await getAllCategories();
+ 
+  return (
+   <div>
+    <GoodFridayBanner />
+   
+   <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4 ">
+    <ProductsView 
+    products={products} categories={categories}/>
+   </div>
+
+   </div>
+   
+  )
+}
